9fe086062cae33c5cecf16297a3965aa45c13404
siploh <siploh.software@gmail.com>
2026-08-09 20:47:02 +0700

    Add files via upload

---
+++ Login/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'login-pwa-v1';
+const assetsToCache = [
+  'Login/index.html',
+  'img/Logo.jpeg'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(assetsToCache);
+      })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
